812f6a4eb55840f40b515ab12b41596a450beb61
Daniel Marques <98705931+Marquezdan@users.noreply.github.com>
2023-04-10 00:23:16 -0300

    Create e Inserts inseridos

--- Arquivos-SQL/SQL1.sql
+++ Arquivos-SQL/SQL1.sql
@@ -22,7 +22,7 @@ CREATE TABLE TB_FACILITADORES
  ID_Departamento INT NOT null,
  PRIMARY KEY(ID_Facilitador),
  FOREIGN KEY (ID_Departamento) REFERENCES TB_DEPARTAMENTOS(ID_Departamento),
- FOREIGN KEY (ID_Salario) REFERENCES TB_SALARIOS(ID_Salario));														   ;
+ FOREIGN KEY (ID_Salario) REFERENCES TB_SALARIOS(ID_Salario));												
 
  -- Cria a tabela dos departamentos.
 CREATE TABLE TB_DEPARTAMENTOS
@@ -113,26 +113,26 @@ CREATE TABLE TB_CURSOS_MODULOS
   
  
   INSERT INTO TB_ALUNOS (Matricula_Aluno, Nome_Aluno, Data_Matricula, Nome_Responsavel, Data_Nasc, Endereco_Aluno, Telefone, Cpf_Responsavel) VALUES 
-(1, 'Cleiton  Silva', '2022-01-10 14:30:00', 'Maria Silva', '2008-05-10 00:00:00', 'Rua das Flores, 123', '98854331', '123.874.778-55'),
-(2, 'Maria da Silva Santos', '2022-01-12 09:00:00', 'José Santos', '2009-03-12 00:00:00', 'Rua dos Pássaros, 456', '995416238', '222.222.222-22'),
-(3, 'Carlos Rodrigues', '2022-01-15 13:15:00', 'Ana Rodrigues', '2007-06-15 00:00:00', 'Avenida Ricardo Moura, 789', '999999997', '333.333.333-33'),
-(4, 'Ana Oliveira', '2022-01-17 10:30:00', 'Paulo Oliveira', '2008-11-17 00:00:00', 'Rua dos Jardins, 1011', '999999996', '444.444.444-44'),
-(5, 'Lucas Costa', '2022-01-20 14:00:00', 'Marta Costa', '2007-01-20 00:00:00', 'Rua dos Sonhos, 1213', '999999995', '555.555.555-55'),
-(6, 'Luana Pereira', '2022-01-22 16:45:00', 'Fernando Pereira', '2009-04-22 00:00:00', 'Avenida dos Ventos, 1415', '999999994', '666.666.666-66'),
-(7, 'Fábio Souza', '2022-01-25 08:30:00', 'Tatiana Souza', '2008-07-25 00:00:00', 'Rua das Oliveiras, 1617', '999999993', '777.777.777-77'),
-(8, 'Larissa Oliveira', '2022-01-28 15:00:00', 'Pedro Oliveira', '2007-03-28 00:00:00', 'Avenida das Águas, 1819', '999999992', '888.888.888-88'),
-(9, 'Pedro Rodrigues', '2022-01-30 11:15:00', 'Luciana Rodrigues', '2008-05-30 00:00:00', 'Rua Uranos, 2021', '999999991', '999.999.999-99'),
-(10, 'Tatiane Costa', '2022-02-02 14:45:00', 'Sérgio Costa', '2009-02-02 00:00:00', 'Avenida Kenedy, 2223', '999999990', '000.000.000-00'),
-(11, 'Larissa Silva', '2022-02-11 14:35:00', 'Luciana Silva', '2008-05-28 00:00:00', 'Rua das Flores, 123', '998887777', '111.222.333-44'),
-(12, 'Lucas Santos', '2022-03-15 10:20:00', 'Marcela Santos', '2009-11-03 00:00:00', 'Rua do Sol, 456', '988886666', '222.333.444-55'),
-(13, 'Rafaela Oliveira', '2022-03-18 16:45:00', 'Mariana Oliveira', '2007-09-21 00:00:00', 'Av. Central, 789', '997778888', '333.444.555-66'),
-(14, 'Gabriel Costa', '2022-04-02 09:10:00', 'Fernanda Costa', '2006-12-10 00:00:00', 'Rua do Mar, 987', '988889999', '444.555.666-77'),
-(15, 'Mariana Lima', '2022-04-05 13:25:00', 'Isabela Lima', '2008-01-17 00:00:00', 'Rua da Praia, 654', '996665555', '555.666.777-88'),
-(16, 'Bruno Ferreira', '2022-05-11 18:30:00', 'Cristina Ferreira', '2009-04-29 00:00:00', 'Rua dos Passarinhos, 321', '988887777', '666.777.888-99'),
-(17, 'Ana Carolina Souza', '2022-06-03 11:05:00', 'Juliana Souza', '2006-07-23 00:00:00', 'Rua da Montanha, 147', '999998888', '777.888.999-00'),
-(18, 'Fernando Santos', '2022-06-15 14:55:00', 'Mariana Santos', '2007-10-12 00:00:00', 'Av. das Flores, 258', '997777888', '888.999.000-11'),
-(19, 'Camila Oliveira', '2022-06-28 09:45:00', 'Patricia Oliveira', '2008-09-06 00:00:00', 'Rua do Céu, 753', '999998888', '999.000.111-22'),
-(20, 'Gustavo Silva', '2022-07-10 16:15:00', 'Amanda Silva', '2009-05-19 00:00:00', 'Rua dos Coqueiros, 159', '988887777', '000.111.222-33');
+(1, 'Diego Castro Araujo', '2022-01-10 14:30:00', 'Maria Castro Araujo', '2008-05-10 00:00:00', 'Rua Joazeiro, 1337', '987543310', '772.181.264-05'),
+(2, 'Matias Valente Ramos', '2022-01-12 09:00:00', 'José Vanlente Ramos', '2009-03-12 00:00:00', 'Rua Boa Vista, 1210', '995416238', '254.151.527-84'),
+(3, 'Carlos Rodrigues', '2022-01-15 13:15:00', 'Ana Rodrigues', '2007-06-15 00:00:00', 'Avenida Ricardo Moura, 789', '985241201', '980.841.148-75'),
+(4, 'Ana Oliveira', '2022-01-17 10:30:00', 'Paulo Oliveira', '2008-11-17 00:00:00', 'Rua dos Jardins, 1011', '986958520', '882.135.590-02'),
+(5, 'Lucas Costa', '2022-01-20 14:00:00', 'Marta Costa', '2007-01-20 00:00:00', 'Rua Santo Agostinho, 1851', '979635410', '394.637.113-25'),
+(6, 'Tânia Almeida Pereira', '2022-01-22 16:45:00', 'Fernando Almeida Pereira', '2009-04-22 00:00:00', 'Rua Cassiano Lemos, 1512', '979867520', '157.317.275-89'),
+(7, 'Rayssa Saraiva Narvais', '2022-01-25 08:30:00', 'Tatiana Saraiva Narvais', '2008-07-25 00:00:00', 'Rua das Oliveiras, 1617', '986947418', '203.472.613-88'),
+(8, 'Larissa Oliveira', '2022-01-28 15:00:00', 'Pedro Oliveira', '2007-03-28 00:00:00', 'Avenida das Águas, 1819', '983432312', '065.855.827-72'),
+(9, 'Pedro Rodrigues', '2022-01-30 11:15:00', 'Luciana Rodrigues', '2008-05-30 00:00:00', 'Rua Uranos, 2021', '988484578', '422.554.307-20'),
+(10, 'Nicole Lima Oliveira', '2022-02-02 14:45:00', 'Sérgio Lima Oliveira', '2009-02-02 00:00:00', 'Avenida Kenedy, 2223', '991708564', '787.585.442-32'),
+(11, 'Carlos Carvalho Souza', '2022-02-11 14:35:00', 'Luciana Carvalho Souza', '2008-05-28 00:00:00', 'Rua Belo Horizonte, 441', '979854731', '665.360.867-08'),
+(12, 'Lucas Santos', '2022-03-15 10:20:00', 'Marcela Santos', '2009-11-03 00:00:00', 'Rua João Paulo VI, 255', '992537456', '762.863.589-00'),
+(13, 'Rafaela Oliveira', '2022-03-18 16:45:00', 'Mariana Oliveira', '2007-09-21 00:00:00', 'Av. Central, 789', '972488244', '162.364.056-38'),
+(14, 'Gabriel Costa', '2022-04-02 09:10:00', 'Fernanda Costa', '2006-12-10 00:00:00', 'Rua Geraldo Macedo, 32', '993574528', '429.903.573-94'),
+(15, 'Lara Barbosa Gomes', '2022-04-05 13:25:00', 'Isabela Barbosa Gomes', '2008-01-17 00:00:00', 'Rua Capitão Vieira, 1133', '999956812', '123.557.633-72'),
+(16, 'Luiz Santos Goncalves', '2022-05-11 18:30:00', 'Cristina Santos Goncalves', '2009-04-29 00:00:00', 'Rua Santa Lúcia, 1864', '985530139', '193.548.163-04'),
+(17, 'Ana Carolina Souza', '2022-06-03 11:05:00', 'Juliana Souza', '2006-07-23 00:00:00', 'Rua Júlio Ribeiro, 1461', '982699078', '468.827.348-79'),
+(18, 'Fernando Santos', '2022-06-15 14:55:00', 'Mariana Santos', '2007-10-12 00:00:00', 'Av. das Flores, 258', '983134598', '630.631.424-50'),
+(19, 'Camila Oliveira', '2022-06-28 09:45:00', 'Patricia Oliveira', '2008-09-06 00:00:00', 'Rua Marechal Deodoro, 863', '988398714', '876.644.351-29'),
+(20, 'Mateus Ferreira Alves', '2022-07-10 16:15:00', 'Amanda Ferreira Alves', '2009-05-19 00:00:00', 'Rua Santo Antônio, 1175', '975453511', '839.022.928-57');
 
 INSERT INTO TB_FACILITADORES (ID_Facilitador, Materia, Nome_Facilitador, Endereco, Telefone, Horario_de_Trabalho, ID_Salario, ID_Departamento) VALUES
 (1, 'Kanban e Scrum', 'Pedro Gomes', 'Rua 4, 456', '95553159', '8:00-17:00', 1, 1),
@@ -145,32 +145,83 @@ INSERT INTO TB_DEPARTAMENTOS (ID_Departamento, Nome_Dep, Descricao)VALUES
 (2,'Departamento Financeiro','Departamento responável pelo controle de finanças e pagamentos'),
 (3,'Departamento Administrativo','Responsável pelo controle e administração dos outros departamentos, assim como documentação');
   
-INSERT INTO TB_CURSOS (ID_Curso, Nome_Curso, Descricao_Curso, Total_Horas, horario)
-VALUES (1, 'Curso em Análise de dados', 'Descrição do curso 1', '100 horas', 'Segunda-feira, 18:00-22:00')  
+INSERT INTO TB_CURSOS (ID_Curso, Nome_Curso, Descricao_Curso, Total_Horas, horario) VALUES 
+(1, 'Curso em Análise de dados', 'Curso com foco na linguagem Python e metodologias ágeis.', '600 horas', '8:00-12:00'),
+(2,'Cuso de Formação JAVA','Curso com foco em JAVA e desenvolvimento de soft skills.', '700 horas', '13:00-17:00');
 
-INSERT INTO TB_MODULOS (ID_Modulo, nome, descricao)
-VALUES (1, 'Módulo 1', 'Descrição do módulo 1');
+INSERT INTO TB_MODULOS (ID_Modulo, nome_modulo , descricao) VALUES 
+(1, 'Python ', 'Principais Noções em Python.'),
+(2,'SQL', 'Criação de bancos e pesquisas utilizando as Querys.'),
+(3,'Metodologias ágeis','Noções e aplicações das principais metodologias ágeis em alta no mercado.'),
+(4,'JAVA','Principais Noções em Python.'),
+(5,'Comunicação e informação','Boas práticas na comunicação e tratamento de informações.');
 
-INSERT INTO TB_CONTADORES (ID_contador, nome, cpf, endereco, data_nasc, ID_Departamento, ID_Salario)
-VALUES (1, 'José', '98765432100', 'Rua 3, 789', '1980-01-01', 1, 2);
+INSERT INTO TB_CONTADORES (ID_contador, nome, cpf, endereco, data_nasc, ID_Departamento, ID_Salario) VALUES 
+(1, 'Carlos Magno', '966.787.231-81', 'Rua Veigas, 19', '1978-03-01', 2, 4),
+(2, 'Jenifer Souza Mello','186.227.454-87', 'Rua Ricardo Soares, 123', '1989-04-12', 2, 4);
 
-INSERT INTO TB_GERENTES (ID_Gerente, nome, cpf, endereco, data_nasc, ID_Departamento, ID_Salario)
-VALUES (1, 'Ana', '11122233344', 'Rua 4, 1010', '1990-01-01', 2, 3);
+INSERT INTO TB_GERENTES (ID_Gerente, nome, cpf, endereco, data_nasc, ID_Departamento, ID_Salario) VALUES 
+(1, 'Ana Paula Gomes', '553.124.787-23', 'Rua Apollo, 1010', '1993-01-09', 3, 5);
 
-INSERT INTO TB_SALARIOS (ID_SALARIO, Valor)
-VALUES (1, 1000.00);
+INSERT INTO TB_SALARIOS (ID_SALARIO, Valor)VALUES 
+(1,2600.00),
+(2,3300.00),
+(3,3600.00),
+(4,3800.00),
+(5,6300.00);
+INSERT INTO TB_ALUNOS_CURSOS (Matricula_Aluno, ID_Curso) VALUES 
+(1, 1),
+(2, 2),
+(3, 1),
+(4, 2),
+(5, 2),
+(6, 1),
+(7, 2),
+(8, 2),
+(9, 1),
+(10,1),
+(11,1),
+(12,1),
+(13,2),
+(14,1),
+(15,2),
+(16,1),
+(17,2),
+(18,2),
+(19,1),
+(20,1);
 
-INSERT INTO TB_ALUNOS_CURSOS (Matricula_Aluno, ID_Curso)
-VALUES (1, 1);
+INSERT INTO TB_MODULOS_FACILITADORES (ID_Facilitador, ID_Modulo)VALUES 
+(1, 3),
+(2, 1),
+(2, 2),
+(3, 4),
+(4, 5);
 
-INSERT INTO TB_MODULOS_FACILITADORES (ID_Facilitador, ID_Modulo)
-VALUES (1, 1);
  
-INSERT INTO TB_CURSOS_MODULOS (ID_Curso, ID_Modulo)
-VALUES (1, 1);
+INSERT INTO TB_CURSOS_MODULOS (ID_Curso, ID_Modulo) VALUES 
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5);
 
-INSERT INTO TB_CONTADORES_SALARIOS (ID_Contador, ID_Salario)
-VALUES (1, 1); 
+INSERT INTO TB_CONTADORES_SALARIOS (ID_Contador, ID_Salario) VALUES 
+(1, 1), 
+(1, 2),  
+(1, 3),  
+(1, 4),
+(1, 5),
+(2, 1), 
+(2, 2),  
+(2, 3),  
+(2, 4),
+(2, 5);
+
+
+
+
+
   
   
   
